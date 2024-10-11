8c41f1db57885514ab0ca67c9b669e224b81c4c6
Tom Bleek <tom.bleek@proton.me>
2024-10-11 09:55:22 +0200

    Small ui adjustment

--- src/cyborg/components/accessCompute/ServiceCard.js
+++ src/cyborg/components/accessCompute/ServiceCard.js
@@ -1,16 +1,21 @@
+import { useState } from "react"
+
 const ServiceCard = ({
   logo,
   title,
   description = '',
-  setService,
-  service = null,
   additionalClasses,
   onClick,
 }) => {
+
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div
-      className={`${additionalClasses} border hover:ring-2 hover:ring-cb-gray-400 border-cb-gray-400 rounded-md w-80`}
-      onClick={onClick}
+      className={`${additionalClasses} border ${isHovered ? 'hover:ring-2 hover:ring-cb-green border-cb-green' : ''} border-cb-gray-400 rounded-md w-80 hover:cursor-pointer`}
+      onMouseDown={onClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
       <div className="flex flex-col items-center focus:text-cb-green bg-cb-gray-600 h-full justify-between">
         <a className="pt-10">
@@ -21,7 +26,7 @@ const ServiceCard = ({
         <p className="text-white opacity-50 text-center text-sm">
           {description}
         </p>
-        <div className="grid justify-center items-center text-lg font-bold opacity-50 bg-cb-gray-400 w-full h-14">
+        <div className={`text-gray-400 grid justify-center items-center text-lg font-bold bg-cb-gray-400 w-full h-14 ${isHovered ? 'bg-cb-green text-cb-gray-400' : ''}`}>
           Deploy Now
         </div>
       </div>
