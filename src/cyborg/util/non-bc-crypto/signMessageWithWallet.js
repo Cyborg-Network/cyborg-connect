ffdb5dcba7aba9bd538d1797c7680c472e2dc0d7
Tom Bleek <tom.bleek@proton.me>
2024-10-23 14:48:59 +0200

    Added talisman check to sign message function

--- src/cyborg/util/non-bc-crypto/signMessageWithWallet.js
+++ src/cyborg/util/non-bc-crypto/signMessageWithWallet.js
@@ -1,10 +1,25 @@
-import { web3FromAddress } from '@polkadot/extension-dapp'
+import { web3FromAddress, web3Enable, web3Accounts } from '@polkadot/extension-dapp'
 import { stringToU8a, u8aToHex } from '@polkadot/util'
 import { toast } from 'react-hot-toast'
 
 export const signMessageWithWallet = async () => {
   try{
-    const address = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
+    const extensions = await web3Enable('Your App Name');
+    
+    if (extensions.length === 0) {
+      throw new Error("No wallet extension found. Please install Polkadot.js or Talisman.");
+    }
+
+    const accounts = await web3Accounts();
+
+    if (accounts.length === 0) {
+      throw new Error('No accounts found. Please ensure you have an account in your wallet.');
+    }
+
+    // Get the first account (or modify logic here for other specific account selections)
+    const selectedAccount = accounts[0];
+
+    const address = selectedAccount.address
 
     const injector = await web3FromAddress(address)
 
